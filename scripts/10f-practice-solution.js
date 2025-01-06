b9d4148949f7921641cfeb0078e380a14d693c5e
Edward Plothow <edplothow@gmail.com>
2025-01-06 15:23:19 -0700

    1/6/2025 2nd practice

--- scripts/10f-practice-solution.js
+++ scripts/10f-practice-solution.js
@@ -1,15 +1,24 @@
 function toggleButton (buttonType) {
-  console.log("bilboBaggins");
+  
 
   const buttonToBeChanged = document.querySelector(buttonType);
-  if (buttonToBeChanged.classList.contains('toggle-button')) {
-    console.log('FrodoBaggins');
-    buttonToBeChanged.classList.add('is-toggled');
-    buttonToBeChanged.classList.remove('toggle-button');
+  if (buttonToBeChanged.classList.contains('grey-button')) {
+
+    turnOffPreviousButton();
+    buttonToBeChanged.classList.add('black-button');
+    buttonToBeChanged.classList.remove('grey-button');
+    
     
   } else {
-    console.log('gandalf the grey');
-    buttonToBeChanged.classList.add('toggle-button');
-    buttonToBeChanged.classList.remove('is-toggled');
+    
+    buttonToBeChanged.classList.add('grey-button');
+    buttonToBeChanged.classList.remove('black-button');
+  }
+  function turnOffPreviousButton() {
+    const previousButton = document.querySelector('.black-button');
+    if (previousButton) {
+      previousButton.classList.remove('black-button');
+      previousButton.classList.add('grey-button');
+    }
   }
 }
