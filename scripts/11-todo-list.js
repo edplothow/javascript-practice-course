3b48906493f2093dfdfa70f44e28efc4d6a59426
Edward Plothow <edplothow@gmail.com>
2025-01-11 22:33:07 -0700

    1/11/25 practice

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,8 @@
-const todoList = ['make dinner', 'wash dishes'];
+
+const todoList = [
+{name:'make dinner', dueDate: '2022-12-22'},
+{name: 'wash dishes', dueDate: '2022-12-22'}
+];
 
 renderTodoList();
 
@@ -6,8 +10,18 @@ function renderTodoList () {
   let todoListHTML = '';
 
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}<button>Delete</button></p>`;
+    const todoObject = todoList[i];
+    const name = todoObject.name;
+    const dueDate = todoObject.dueDate;
+    
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick = "
+      todoList.splice(${i}, 1);
+      renderTodoList();
+    "class="delete-todo-button">Delete</button>
+  `;
     todoListHTML += html;
   }
 
@@ -20,7 +34,13 @@ function renderTodoList () {
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
-  todoList.push(name);
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push(
+    {name: name, dueDate: dueDate}
+  );
   
 
   inputElement.value = '';
